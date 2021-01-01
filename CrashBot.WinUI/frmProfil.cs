6cc7f569888574b7fa59f4a21a0f6d959c84d284
Semir Klapuh <semirklapuh@gmail.com>
2021-01-01 17:07:24 +0100

    Initial commit

---
+++ CrashBot.WinUI/frmProfil.cs
@@ -0,0 +1,100 @@
+﻿using CrashBot.Model;
+using CrashBot.Model.Requests;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrashBot.WinUI
+{
+    public partial class frmProfil : Form
+    {
+        private readonly APIService _korisnik = new APIService("Korisnik");
+        public frmProfil()
+        {
+            InitializeComponent();
+        }
+
+        private async void frmProfil_Load(object sender, EventArgs e)
+        {
+            var korisnik = await _korisnik.GetById<Model.Korisnik>(Global.prijavljeniKorisnik.KorisnikId);
+
+            txtIme.Text = korisnik.Ime;
+            txtPrezime.Text = korisnik.Prezime;
+            txtAdresa.Text = korisnik.Adresa;
+            txtKorisnickoIme.Text = korisnik.KorisnickoIme;
+            txtMail.Text = korisnik.Mail;
+            txtTelefon.Text = korisnik.Telefon;
+            txtLozinka.Text = korisnik.Lozinka;
+            
+
+        }
+
+        private async void btnSnimi_Click(object sender, EventArgs e)
+        {
+
+            if (Validacija())
+            {
+                MessageBox.Show("Ispravno unesite podatke.", "Info", MessageBoxButtons.OK);
+            }
+            else
+            {
+
+                if (txtNovaLozinka.Text != "")
+                {
+                    txtLozinka.Text = txtNovaLozinka.Text;
+                }
+                KorisnikUpsertRequest model = new KorisnikUpsertRequest()
+                {
+                    Ime = txtIme.Text,
+                    Prezime = txtPrezime.Text,
+                    Adresa = txtAdresa.Text,
+                    KorisnickoIme = txtKorisnickoIme.Text,
+                    Mail = txtMail.Text,
+                    Telefon = txtTelefon.Text,
+                    Lozinka = txtLozinka.Text,                   
+                };
+
+                var nesto = await _korisnik.Update<Model.Korisnik>(Global.prijavljeniKorisnik.KorisnikId, model);
+                MessageBox.Show("Uspješno promijenjeni podaci!", "Info", MessageBoxButtons.OK);
+            }
+            
+
+            
+        }
+
+        private bool Validacija()
+        {
+            if (txtIme.Text == "" || txtPrezime.Text == "")
+            {
+                errorProvider1.SetError(txtIme, "Ime je obavezno polje");
+                errorProvider1.SetError(txtPrezime, "Prezime je obavezno polje");
+
+                return true;
+            }
+            if (txtKorisnickoIme.Text == "")
+            {
+                errorProvider1.SetError(txtKorisnickoIme, "Korisnicko ime je obavezno polje");
+                return true;
+            }
+            if (txtNovaLozinka.Text != txtPonovljenaLozinka.Text)
+            {
+                errorProvider1.SetError(txtNovaLozinka, "Neispravno unesena lozinka");
+                errorProvider1.SetError(txtPonovljenaLozinka, "Neispravno unesena lozinka");
+                return true;
+            }
+
+            return false;
+        }
+
+        private void btnNazad_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ CrashBot.WinUI/frmSignin.cs
@@ -0,0 +1,78 @@
+﻿using CrashBot.Model.Requests;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrashBot.WinUI
+{
+    public partial class frmSignin : Form
+    {
+        private readonly APIService _korisnik = new APIService("Korisnik");
+        public frmSignin()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            if (Validacija())
+            {
+                MessageBox.Show("Ispravno unesite podatke.", "Info", MessageBoxButtons.OK);
+            }
+            else
+            {
+                KorisnikUpsertRequest model = new KorisnikUpsertRequest();
+              
+                    model.Ime = txtIme.Text;
+                    model.Prezime = txtPrezime.Text;
+                    model.Mail = txtMail.Text;
+                    model.KorisnickoIme = txtKorisnickoIme.Text;
+                    model.Adresa = txtAdresa.Text;
+                    model.Telefon = txtTelefon.Text;
+                    model.Lozinka = txtLozinka.Text;
+
+                    var nesto = await _korisnik.Insert<Model.Korisnik>(model);
+
+                    MessageBox.Show("Uspjesna registracija.", "Info", MessageBoxButtons.OK);
+                    Close();
+               
+            }
+            
+           
+            
+
+            
+            
+        }
+
+        private bool Validacija()
+        {
+            if (txtIme.Text == "" || txtPrezime.Text =="")
+            {
+                errorProvider1.SetError(txtIme, "Ime je obavezno polje");
+                errorProvider1.SetError(txtPrezime, "Prezime je obavezno polje");
+
+                return true;
+            }
+            if (txtKorisnickoIme.Text == "")
+            {
+                errorProvider1.SetError(txtKorisnickoIme, "Korisnicko ime je obavezno polje");
+                return true;
+            }
+            if (txtLozinka.Text != txtPonovljenaLozinka.Text)
+            {
+                errorProvider1.SetError(txtLozinka, "Neispravno unesena lozinka");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
